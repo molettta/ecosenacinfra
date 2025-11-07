556b689557d8ded2a74ddea215d8ff878d1a786e
Gisele Slusarz <giseleslusarz07@gmail.com>
2025-11-07 15:51:04 -0300

    Fix Laravel routing bootstrap in Docker

--- laravel/app/config/app.php
+++ laravel/app/config/app.php
@@ -20,6 +20,7 @@ return [
     ],
     'providers' => ServiceProvider::defaultProviders()->merge([
         App\Providers\AppServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
     ])->toArray(),
     'aliases' => Facade::defaultAliases()->merge([
     ])->toArray(),
